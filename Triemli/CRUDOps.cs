6b9a4138144cfb3d5236305ce614ccbea7f68b9d
dmurphi <dmurphi@yahoo.com>
2014-12-12 20:59:02 +0200

    Adding initial db abstraction

---
+++ Triemli/CRUDOps.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triemli
+{
+    public class CrudOps
+    {
+        #region member variables
+
+        private static PetaPoco.Database _currentDb;
+
+        #endregion
+
+        #region constructors
+
+        public CrudOps(PetaPoco.Database _db)
+        {
+            _currentDb = _db;
+        }
+
+        #endregion
+
+
+        #region db operations
+        ////// use parameterised queries like this:
+        //////        db.Query<MyTable>("WHERE MyColumn=@myValue", new {myValue = "test"})
+        ////// for better performance
+
+
+        public object GetData(string select, string from, string whereConditions)
+        {
+            var sql = PetaPoco.Sql.Builder
+                                            .Select("SELECT @0", select)
+                                            .From("@0",from)
+                                            .Where("@0", whereConditions);
+
+            var product = _currentDb.Fetch<object>(sql);
+            return product;
+        }
+
+
+        public int UpdateData(object pocoObject)
+        {
+            return _currentDb.Update(pocoObject);
+        }
+
+
+        public int DeleteData(object pocoObject)
+        {
+            return _currentDb.Delete(pocoObject);
+        }
+
+        public void Insert(object pocoObject)
+        {
+            _currentDb.Insert(pocoObject);
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ Triemli/DBConnection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triemli
+{
+    public class DBConnection
+    {
+        #region member variables
+        
+        private PetaPoco.Database _db;
+        private static string _connectionString;
+        private static string _provider;
+        
+        #endregion
+
+
+        #region constructors
+
+        public DBConnection(string connString)
+        {
+            _connectionString = connString;
+            if (_db == null || _db.Connection.ConnectionString.Trim().ToLower() != _connectionString.Trim().ToLower())
+            {
+                _db = new PetaPoco.Database(_connectionString);
+            }
+        }
+
+        public DBConnection(string connString, string provider)
+        {
+            _connectionString = connString;
+            _provider = provider;
+            if (_db == null || _db.Connection.ConnectionString.Trim().ToLower() != _connectionString.Trim().ToLower())
+            {
+                _db = new PetaPoco.Database(_connectionString, _provider);
+            }
+        }
+
+        #endregion
+
+        #region public methods
+
+        public PetaPoco.Database currentDatabase
+        {
+            get
+            {
+                return _db;
+            }
+        }
+
+        #endregion
+    }
+}
